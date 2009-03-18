85deff9262e4be52e25f2a7975c7c0353b37a34d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-03-18 13:43:19 +0900

    initial commit

---
+++ lib/HTML/CSSInliner.pm
@@ -0,0 +1,33 @@
+package HTML::CSSInliner;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::CSSInliner -
+
+=head1 SYNOPSIS
+
+  use HTML::CSSInliner;
+
+=head1 DESCRIPTION
+
+HTML::CSSInliner is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom @*(#RJKLFHFSDLJF gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
